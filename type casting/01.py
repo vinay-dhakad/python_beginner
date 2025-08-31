be84dc46e08795fb06f7c0bb9c0da8b08b4db3ad
vinay-dhakad <vinaydhakad1409@gmail.com>
2025-08-31 19:43:25 +0530

    type casting

---
+++ type casting/01.py
@@ -0,0 +1,22 @@
+a = int("2")
+b = 4.55
+
+print(type(a))
+print(a+b)
+
+# a = float("arun")
+# b = 4.55
+
+# print(type(a))
+# print(a+b)
+
+a = float("5") 
+b = 4.55
+
+print(type(a))
+print(a+b)
+
+a = 3.34
+a = str(a)
+
+print(type(a))
