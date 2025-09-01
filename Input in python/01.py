7624e5b87f2c355a425aba0260001e36cb7fab7d
vinay-dhakad <vinaydhakad1409@gmail.com>
2025-09-01 22:42:52 +0530

    Input in python

---
+++ Input in python/01.py
@@ -0,0 +1,19 @@
+# name = input("enter your name ")
+# print("Welcome ", name )
+
+
+# int("5")
+# val = int(input("enter some value: "))
+# print(type(val), val)  
+
+
+# int("5")
+# val = float(input("enter some value: "))
+# print(type(val), val)  
+
+
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks: ")
+
+print("welcome", name )
