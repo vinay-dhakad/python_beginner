a5df90f547a0ce60e4196dd52c67f66cbec50bf3
vinay-dhakad <vinaydhakad1409@gmail.com>
2025-09-02 19:32:32 +0530

    practice que

---
+++ practice que.py
@@ -0,0 +1,27 @@
+#write a program to input 2 numbers and print their sum.
+# x = int(input("enter first : "))
+# y = int(input("enter second : "))
+
+# sum = x+y
+# print(sum)
+
+#write a program to input side of square and print its area.
+# side = float(input("enter square side: "))
+
+# print("area =" , side * side )
+
+#write a program to input 2 floating point numbers and write its average.
+
+# a = float(input("enter first : "))
+# b = float(input("enter second : "))
+
+# print("average = " , a+b/2 )
+
+#write a program to input 2 int numbers, a and b.
+# Print TRUE if a is greater than or equal to b. If not print FALSE.
+
+a = int(input("enter first : "))
+b = int(input("enter second : "))
+
+print(a>=b)
+print(a<b)
