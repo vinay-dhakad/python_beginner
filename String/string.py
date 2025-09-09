eb090c3116e706d6c8fea1a1723bc5ea8d737687
vinay-dhakad <vinaydhakad1409@gmail.com>
2025-09-09 19:10:10 +0530

    message

--- String/string.py
+++ String/string.py
@@ -4,3 +4,15 @@
 
 str0 = "this is a string.\n i am President of America."
 print(str0)
+
+
+str1 = "apna"   #concatenation
+str2 = "college"
+final_str = str1 + str2
+print(final_str)
+
+str1 = "apna"
+print(len(str1))   #lenght of str
+str2 = "college"
+final_str = str1 + str2
+print(final_str)
