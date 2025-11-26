567c4c54953c33610ec66d7d357cf07f8cd0e8fc
reactranger01 <reactranger01@gmail.com>
2025-11-26 19:07:58 +0530

    dictionaries

---
+++ python/dictionaries/dictionaries.py
@@ -0,0 +1,37 @@
+# info = {
+
+# "name" : "ronaldo",
+# "age" : 90,
+# "marks" : [12,2,4]
+# }
+# print(info)
+
+# info = {
+
+# "name" : "ronaldo",
+# "subjects" :["python","c","c++"],
+# "age" : 90,
+# "is_adult" : False,
+# "marks" : [12,2,4],          #dictionary is mutable,like list.
+# }
+# print(type(info))
+# print(info)
+
+
+# null_dict = {}
+# null_dict["name"] = "messi"
+# print(null_dict)
+
+#NESTED DICTIONARY
+student = {
+    "name" : "Temba Bauma",
+    "subjects": {
+        "physics":90,
+        "hindi" : 81,
+        "sanskrit" : 0,
+
+
+    }
+}
+print(student)
+print(student["subjects"])
