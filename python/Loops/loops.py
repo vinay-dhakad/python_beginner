6cf67cc6191a33a3ead3fa465861cd9f7e21b6ef
reactranger01 <reactranger01@gmail.com>
2025-12-03 19:08:33 +0530

    loops

---
+++ python/Loops/loops.py
@@ -0,0 +1,23 @@
+# count = 1                 #variable (count) is called iterator, and process is called iteration. 
+# while count <=5 :
+#     print("HELLO")
+#     count += 1
+
+# i = 1
+# while i<=10 :
+#     print("EGO")
+#     i +=1
+
+
+i = 1
+while i<=10 :
+    print("EGO" , i)
+    i +=1
+
+
+#print numbers from 1 to 5 
+
+i = 1
+while i<= 5:
+    print (i)
+    i +=1
---
+++ python/Loops/practice.py
@@ -0,0 +1,22 @@
+#print the no. 1 to 100
+# i = 1 
+# while i <= 100:
+#     print (i)
+#     i += 1
+
+#print no. from 100 to 1.
+# i = 100
+# while i >= 1:
+#     print (i)
+#     i -= 1
+
+#print multiplication table of no. n
+# 1
+
+#print the elements of following list using the loop:
+[1,4,9,16,25,36,49,64,81,100]
+
+n = 1
+while n<= 10:
+    print(n*n)
+    n +=1
