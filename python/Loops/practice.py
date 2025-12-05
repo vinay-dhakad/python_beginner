12bd4545bcbec42b08635cad931a40de7a389178
reactranger01 <reactranger01@gmail.com>
2025-12-05 19:28:50 +0530

    loops practice

--- python/Loops/practice.py
+++ python/Loops/practice.py
@@ -14,9 +14,34 @@
 # 1
 
 #print the elements of following list using the loop:
-[1,4,9,16,25,36,49,64,81,100]
+# [1,4,9,16,25,36,49,64,81,100]
 
-n = 1
-while n<= 10:
-    print(n*n)
-    n +=1
+# nums = [1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1 
+
+# traverse
+# heroes = ["ironman","thor","captain","hulk","kedar jadav"]
+# idx = 0
+# while idx < len(heroes):
+#     print(heroes[idx])
+#     idx += 1
+
+
+#search for no. x in this tuple using loop:
+# (1,4,9,16,25,36,49,64,81,100)
+
+nums=(1,4,9,16,25,36,49,64,81,100)
+
+
+x=36
+
+i=0
+while i < len(nums):
+    if(nums [i] == x):
+        print("FOUND at idx",i)
+  
+      i += 1
--- python/Loops/tempCodeRunnerFile.py
+++ python/Loops/tempCodeRunnerFile.py
@@ -1,5 +1,10 @@
-n = int(input("enter any value: "))
-# i = 1
-# while i <= 10:
-#     print(n * i)
-#     i += 1
+
+nums=(1,4,9,16,25,36,49,64,81,100)
+x=36
+
+i=0
+while i<len(nums):
+    if(nums [i] ==x):
+        print("FOUND at idx",i)
+  
+      i += 1
